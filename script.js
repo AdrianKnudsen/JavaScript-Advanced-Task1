9050bad8cbaac34436fe476410df85c714c13c42
AdrianKnudsen <adrian@knudsen.no>
2023-09-25 13:38:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const officeQuotes = [ 
+    
+    `And i knew exactly what to do. but in a much more real sense, i had no idea what to do. - Michael Scott` ,
+
+    `Sometimes i'll start a sentence and i don't even know where it's going. i just hope i find it along the way. - Michael Scott`,
+    
+    `I talk a lot, so i've learned to tune myself out. - Kelly Kapoor`,
+    
+    `Would i rather be feared or loved? easy. both. i want people to be afraid of how much they love me. - Michael Scott`,
+    
+    `I'm not superstitious, but i am a little stitious. - Michael Scott`,
+    
+    `No, rose, they are not breathing. and they have no arms or legs … where are they? you know what? if we come across somebody with no arms or legs, do we bother resuscitating them? i mean, what quality of life do we have there? - Michael Scott`,
+    
+    `I am running away from my responsibilities. and it feels good. - Michael Scott`,
+    
+    `And i knew exactly what to do. but in a much more real sense, i had no idea what to do. - Michael Scott`,
+    
+    `Identity theft is not a joke, jim! millions of families suffer every year. - Dwight Schrute`,
+    
+    `I'm such a perfectionist that i'd kinda rather not do it at all than do a crappy version. - Ryan Howard`,
+    
+    `I'm an early bird and i'm a night owl. so i'm wise and i have worms. - Michael Scott`,
+    
+    `Me think, why waste time say lot word when few word do trick. - Kevin Malone`,
+    
+    `Powerpoints are the peacocks of the business world; all show, no meat. - Dwight Schrute`
+
+    ];
+
+    console.log(officeQuotes)
